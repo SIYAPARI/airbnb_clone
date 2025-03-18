82c0093df6dbe16a97ff053cabcb55df58a6f67a
SIYA PARI <siyachandel56@gmail.com>
2025-03-18 22:28:21 +0530

    upload images or files instead of its link

--- routes/listing.js
+++ routes/listing.js
@@ -1,15 +1,19 @@
 const express=require("express");;
 const router=express.Router();
 const wrapasync=require("../util/wrapasync.js");
-
 const Listing=require("../models/listing.js");
 const {validateListing,isLoggedIn,isOwner}=require("../middleware.js");
 const listingController=require("../controllers/listing.js")
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
 .route("/")
 .get(wrapasync(listingController.index))
-.post(isLoggedIn,validateListing,wrapasync(listingController.createListing));
+// .post(isLoggedIn,validateListing,wrapasync(listingController.createListing));
+.post(upload.single('listing[image]'),(req,res)=>{
+    res.send(req.file);
+})
 
 //NEW ROUTE
 router.get("/new",isLoggedIn,listingController.renderNewForm);
